fb72d63f59988561702fab9148caf6230119b463
HamiMH <hamacekm93@gmail.com>
2024-07-24 19:56:34 +0200

    Add training repo, disposable

---
+++ DotNetTraining/001_Dispose/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _001_Dispose
+{
+	/// <summary>
+	/// https://www.bytehide.com/blog/idisposable-method-csharp
+	/// </summary>
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+			WriteText();
+			Console.WriteLine("Read key 2");
+			Console.ReadKey();
+			GC.Collect();
+			Console.WriteLine("Read key 3");
+			Console.ReadKey();
+
+
+		}
+
+		static void WriteTextUsing()
+		{
+			using (TextFileWriter writer = new TextFileWriter())
+			{
+				writer.WriteTextToFile("First text\n");
+				writer.WriteTextToFile("Second text\n");
+				Console.WriteLine("Read key 1");
+				Console.ReadKey();
+				writer.SaveAndReopenStreamWriter();
+				writer.WriteTextToFile("Third text\n");
+				Console.WriteLine("End of WriteText");
+				Console.ReadKey();
+			}
+		}
+		static void WriteText()
+		{
+			TextFileWriter writer = new TextFileWriter();
+			writer.WriteTextToFile("First text\n");
+			writer.WriteTextToFile("Second text\n");
+			Console.WriteLine("Read key 1");
+			Console.ReadKey();
+			writer.SaveAndReopenStreamWriter();
+			writer.WriteTextToFile("Third text\n");
+			Console.WriteLine("End of WriteText");
+			Console.ReadKey();
+			//writer.Dispose();
+			//writer.WriteTextToFile("Fourth text\n"); Cause error
+		}
+	}
+}
---
+++ DotNetTraining/001_Dispose/TextFileWriter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_Dispose
+{
+	public class TextFileWriter : IDisposable
+	{
+		private bool _disposed = false;
+		private StreamWriter _streamWriter;
+		private string _filePath = "textfile.txt";
+
+		public TextFileWriter()
+		{
+			// Open StreamWriter when the object is initiated.
+			_streamWriter = new StreamWriter(_filePath, true);
+		}
+
+		public void WriteTextToFile(string text)
+		{
+			_streamWriter.WriteLine(text);
+		}
+
+		public void SaveAndReopenStreamWriter()
+		{
+			// Close and dispose of the existing StreamWriter
+			_streamWriter.Close();
+			_streamWriter.Dispose();
+
+			// Reopen StreamWriter
+			_streamWriter = new StreamWriter(_filePath, true);
+		}
+
+		private void Dispose(bool disposing)
+		{
+			if (_disposed) 
+				return;
+
+			Console.WriteLine("Dispose() was called with param: "+disposing);
+			// Close and dispose StreamWriter when the object is disposed
+			_streamWriter.Close();
+			_streamWriter.Dispose();
+
+			_disposed = true;
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+		~TextFileWriter()
+		{
+			Dispose(false);
+		}
+	}
+}
